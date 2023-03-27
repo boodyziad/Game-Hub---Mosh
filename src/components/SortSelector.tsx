5be7a85184622742e76e77791433ec02173f461e
Abdallah Ziad <boodyziad1@gmail.com>
2023-03-27 23:24:54 +0200

    Build sort selector

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,25 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { BsChevronDown } from 'react-icons/bs';
+import { Platform } from '../hooks/useGames';
+
+interface Props {}
+
+const SortSelector = ({}: Props) => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order by: Relevance
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Relevance</MenuItem>
+        <MenuItem>Date added</MenuItem>
+        <MenuItem>Name</MenuItem>
+        <MenuItem>Release date</MenuItem>
+        <MenuItem>Popularity</MenuItem>
+        <MenuItem>Average rating</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
